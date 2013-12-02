2e92f7318f99e339209825908ff76f9f915b279c
Trevor Joynson (trevorj) <trevorj@skywww.net>
2013-12-02 16:42:03 -0500

    Add in properties

--- cli.py
+++ cli.py
@@ -98,6 +98,20 @@ class Dataset(_StorageChildBase):
     def __init__(self, parent, obj):
         _StorageChildBase.__init__(self, parent, obj)
 
+        # Dataset config group
+        self.define_config_group_param('dataset', 'compression', 'string', 'Enable compression')
+        self.define_config_group_param('dataset', 'dedup', 'string', 'Enable dedupe')
+
+        self.define_config_group_param('dataset', 'compressratio', 'string', 'Compresstion ratio', writable=False)
+        self.define_config_group_param('dataset', 'used', 'string', 'Used space', writable=False)
+        self.define_config_group_param('dataset', 'usedbysnapshots', 'string', 'Used space by snapshots', writable=False)
+        self.define_config_group_param('dataset', 'usedbydataset', 'string', 'Used space by dataset', writable=False)
+        self.define_config_group_param('dataset', 'usedbychildren', 'string', 'Used space by children', writable=False)
+        self.define_config_group_param('dataset', 'usedbyrefreservation', 'string', 'Used space by referenced reservation', writable=False)
+        self.define_config_group_param('dataset', 'referenced', 'string', 'Referenced space', writable=False)
+        self.define_config_group_param('dataset', 'available', 'string', 'Available space', writable=False)
+        self.define_config_group_param('dataset', 'creation', 'string', 'Creation date', writable=False)
+
     @classmethod
     def _find_subclass_for_type_mask(cls, type_mask):
         for scls in cls.__subclasses__():
@@ -119,7 +133,18 @@ class Dataset(_StorageChildBase):
         return self
 
     def ui_command_destroy(self):
+        """ Destroy Dataset. """
         raise NotImplementedError()
+
+    """ Properties """
+
+    def ui_getgroup_dataset(self, key):
+        """ Get Dataset property. """
+        return self.dataset.props[key]
+
+    def ui_setgroup_dataset(self, key, value):
+        """ Set Dataset property. """
+        self.dataset.props[key] = value
 
 
 class _SnapshottableDataset:
@@ -127,36 +152,103 @@ class _SnapshottableDataset:
         """ Creates a named snapshot. """
         self.dataset.snapshot(name, recursive=recursive)
 
+    def ui_command_destroy_snapshot(self, name, recursive=True):
+        """ Destroys a named snapshot. """
+        self.dataset.destroy_snapshot(name, recursive=recursive)
+
 
 class Filesystem(Dataset, _SnapshottableDataset):
     _zfs_type_mask = ZFilesystem._zfs_type_mask
 
+    def __init__(self, parent, obj):
+        Dataset.__init__(self, parent, obj)
+
+        # Filesystem config group
+        self.define_config_group_param('filesystem', 'atime', 'string', 'Keep file access times up to date')
+        self.define_config_group_param('filesystem', 'quota', 'string', 'Quota for filesystem')
+        self.define_config_group_param('filesystem', 'mounted', 'bool', 'Currently mounted', writable=False)
+
+    # Filesystem config group
+
+    def ui_getgroup_filesystem(self, key):
+        """ Get Filesystem property. """
+        return Dataset.ui_getgroup_dataset(self, key)
+
+    def ui_setgroup_filesystem(self, key, value):
+        """ Set Filesystem property. """
+        return Dataset.ui_setgroup_dataset(self, key, value)
+
+    # Operations
+
     def ui_command_create_filesystem(self, name):
+        """ Create Filesystem. """
         raise NotImplementedError()
 
     def ui_command_create_volume(self, name, size):
+        """ Create Volume. """
         raise NotImplementedError()
 
     def ui_command_ls_snapshots(self):
+        """ List snapshots. """
         for s in self.dataset.iter_snapshots_sorted():
-            print s.name
+            print s.snapshot_name
 
 
 class Volume(Dataset, _SnapshottableDataset):
     _zfs_type_mask = ZVolume._zfs_type_mask
 
-    def ui_command_resize(self, size):
-        raise NotImplementedError()
+    def __init__(self, parent, obj):
+        Dataset.__init__(self, parent, obj)
+
+        # Volume config group
+        self.define_config_group_param('volume', 'volblocksize', 'string', 'Volume Block size', writable=False)
+        self.define_config_group_param('volume', 'volsize', 'string', 'Volume Size')
+
+    # Volume config group
+
+    def ui_getgroup_volume(self, key):
+        """ Get Volume property. """
+        return Dataset.ui_getgroup_dataset(self, key)
+
+    def ui_setgroup_volume(self, key, value):
+        """ Set Volume property. """
+        return Dataset.ui_setgroup_dataset(self, key, value)
 
 
 class Pool(Filesystem):
     _zfs_type_mask = ZPool._zfs_type_mask
 
     def __init__(self, parent, obj):
-        _StorageChildBase.__init__(self, parent, obj)
+        Filesystem.__init__(self, parent, obj)
+
+        # Pool config group
+        self.define_config_group_param('pool', 'comment', 'string', 'Comment')
+        self.define_config_group_param('pool', 'dedupditto', 'string', 'Number of copies of each deduplicated block to save')
+        self.define_config_group_param('pool', 'autoexpand', 'string', 'Automatically expand pool if drives increase in size')
+        self.define_config_group_param('pool', 'autoreplace', 'string', 'Automatically replace failed drives with any specified hot spare(s)')
+
+        #self.define_config_group_param('pool', 'name', 'string', 'Pool Name', writable=False)
+        self.define_config_group_param('pool', 'health', 'string', 'Health', writable=False)
+        self.define_config_group_param('pool', 'dedupratio', 'string', 'Dedupe ratio', writable=False)
+
+        self.define_config_group_param('pool', 'capacity', 'string', 'Percentage filled', writable=False)
+        self.define_config_group_param('pool', 'allocated', 'string', 'Allocated space', writable=False)
+        self.define_config_group_param('pool', 'free', 'string', 'Free space', writable=False)
+        self.define_config_group_param('pool', 'size', 'string', 'Total space', writable=False)
 
     def ui_command_destroy(self):
+        """ Destroy Pool. """
         raise NotImplementedError()
+
+    """ Properties """
+
+    def ui_getgroup_pool(self, key):
+        """ Get Pool property. """
+        return self.pool.props[key]
+
+    def ui_setgroup_pool(self, key, value):
+        """ Set Pool property. """
+        self.pool.props[key] = value
 
 
 def main():
