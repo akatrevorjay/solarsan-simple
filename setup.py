009635bd93ac29796b9e997766348ef5b85130ec
Trevor Joynson (trevorj) <trevorj@skywww.net>
2013-12-03 14:06:30 -0500

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+"""
+Part of SolarSan.
+
+Copyright 2013 Trevor Joynson
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='solarsan',
+    version='0.0.1',
+    description='SolarSan Core',
+    license='GPL3',
+    maintainer='Trevor Joynson',
+    maintainer_email='github@skywww.net',
+    url='http://github.com/akatrevorjay/solarsan',
+    packages=['san'],
+    #use_2to3=True,
+    entry_points=dict(
+        console_scripts=['sancli=san.cli:main'],
+    ),
+)
