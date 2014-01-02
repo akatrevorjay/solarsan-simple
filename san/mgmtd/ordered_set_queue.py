48c3473056114985fdfc22b71c82598bdc5331dd
Trevor Joynson (trevorj) <trevorj@skywww.net>
2014-01-02 16:48:43 -0500

    Add CheckableOrderedSetQueue, but with warnings that it is not safe

--- san/mgmtd/ordered_set_queue.py
+++ san/mgmtd/ordered_set_queue.py
@@ -12,3 +12,11 @@ class OrderedSetQueue(Queue.Queue):
 
     def _get(self):
         return self.queue.pop()
+
+
+class CheckableOrderedSetQueue(OrderedSetQueue):
+    """ This is really not safe, as it allows for race conditions to exist say if the
+        object disappears after it was checked for. """
+    def __contains__(self, item):
+        with self.mutex:
+            return item in self.queue
